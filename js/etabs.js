c7b23fda7dc1f9c39173699ca3146a896c1553af
DFPSetif <dfpsetifcodes.gmail.com>
2021-10-27 14:21:28 +0100

    add the geographic information system of Sétif city

---
+++ js/etabs.js
@@ -0,0 +1,449 @@
+const etabs = [
+	{
+		"type":"DFP",
+		"commune":"سطيف",
+		"denomination":"مديرية التكوين المهني و التعليم المهنيين سطيف",
+		"adresse":"حي القاهرة سطيف",
+		"tel":"0 36 74 34 99",
+		"fax":"0 36 74 34 99",
+		"email":"dfpsetif@gmail.com",
+		"image":"img/dfp.jpg"
+	},
+	{
+		"type":"IFP",
+		"commune":"سطيف",
+		"denomination":"معهد التكوين و التعليم المهنيين سطيف",
+		"adresse":"عين تبينت سطيف",
+		"tel":"0 36 62 11 89",
+		"fax":"0 36 62 11 90",
+		"email":"ifepsetif.mfep@gmail.com",
+		"image":"img/IFPSetif.jpg"
+	},
+	{
+		"type":"INSFP",
+		"commune":"سطيف",
+		"denomination":"المعهد الوطني المتخصص في التكوين المهني سطيف",
+		"adresse":"حي 500 مسكن سطيف",
+		"tel":"0 36 51 51 86",
+		"fax":"0 36 51 51 85",
+		"email":"insfpsetif500.mfep@gmail.com",
+		"image":"img/insfp500.jpg"
+	},
+	{
+		"type":"INSFP",
+		"commune":"سطيف",
+		"denomination":"المعهد الوطني المتخصص في التكوين المهني الهضاب سطيف",
+		"adresse":"حي الهضاب سطيف",
+		"tel":"0 36 54 88 84",
+		"fax":"0 36 54 88 82",
+		"email":"insfpelhidhab.mfep@gmail.com",
+		"image":"img/INSFPhidhab.jpg"
+	},
+	{
+		"type":"INSFP",
+		"commune":"سطيف",
+		"denomination":"المعهد الوطني المتخصص في التكوين المهني عين تبينت",
+		"adresse":"عين تبينت سطيف",
+		"tel":"0 36 66 42 01",
+		"fax":"0 36 66 41 99",
+		"email":"insfpaintbinet.mfep@gmail.com",
+		"image":"img/INSFPaintbinet.jpg"
+	},
+	{
+		"type":"INSFP",
+		"commune":"عين ولمان",
+		"denomination":"المعهد الوطني المتخصص في التكوين المهني عين ولمان",
+		"adresse":"مدخل مدينة عين ولمان",
+		"tel":"036 56 13 13",
+		"fax":"036 56 14 14",
+		"email":"insfpainoulmene.mfep@gmail.com",
+		"image":"img/INSFPoulmene.jpg"
+	},
+	{
+		"type":"INSFP",
+		"commune":"عين آرنات",
+		"denomination":"المعهد الوطني المتخصص في التكوين المهني عين آرنات",
+		"adresse":"عين آرنات",
+		"tel":"036 43 24 02",
+		"fax":"036 43 24 01",
+		"email":"insfpainarnet.mfep@gmail.com",
+		"image":"img/INSFParnat.jpg"
+	},
+	{
+		"type":"IEP",
+		"commune":"سطيف",
+		"denomination":"معهد التعليم المهني سطيف ",
+		"adresse":"حي 1006 مسكن سطيف",
+		"tel":"036 61 95 06",
+		"fax":"036 61 95 08",
+		"email":"iepsetif.mfep@gmail.com",
+		"image":"img/IEP1006.jpg"
+	},
+	{
+		"type":"IEP",
+		"commune":"العلمة",
+		"denomination":"معهد التعليم المهني العلمة ",
+		"adresse":"حي 01 نوفمبر 54 العلمة",
+		"tel":"036 69 13 85",
+		"fax":"036 69 13 86",
+		"email":"iepeleulma.mfep@gmail.com",
+		"image":"img/IEPeulma.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"سطيف",
+		"denomination":"مركز التكوين المهني والتمهين سطيف 03",
+		"adresse":"حي الهضاب سطيف",
+		"tel":"036 63 20 04",
+		"fax":"036 63 20 03",
+		"email":"cfpasetif3.mfep@gmail.com",
+		"image":"img/setif3.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"سطيف",
+		"denomination":"مركز التكوين المهني والتمهين سطيف ذكور",
+		"adresse":"حي الهواء الجميل سطيف",
+		"tel":"036 61 23 61",
+		"fax":"036 61 23 62",
+		"email":"cfpasetifg.mfep@gmail.com",
+		"image":"img/setifg.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"بوقاعة",
+		"denomination":"مركز التكوين المهني والتمهين بوقاعة",
+		"adresse":"حي تالة نتروميت بوقاعة",
+		"tel":"036 40 13 13",
+		"fax":"036 40 13 58",
+		"email":"Cfpabougaa.mfep@gmail.com",
+		"image":"img/bougaa.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"حمام قرقور",
+		"denomination":"مركز التكوين المهني والتمهين حمام قرقور",
+		"adresse":"حمام قرقور",
+		"tel":"036 49 91 29",
+		"fax":"036 49 91 30",
+		"email":"cfpahammamguergour.mfep@gmail.com",
+		"image":"img/hammaguergour.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"جميلة",
+		"denomination":"مركز التكوين المهني والتمهين جميلة",
+		"adresse":"بلدية جميلة",
+		"tel":"036 69 05 35",
+		"fax":"036 69 05 34",
+		"email":"cfpadjemila.mfep@gmail.com",
+		"image":"img/djemila.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"بني عزيز",
+		"denomination":"مركز التكوين المهني والتمهين بني عزيز",
+		"adresse":"شارع صالح رياش بني عزيز",
+		"tel":"036 76 80 06",
+		"fax":"036 76 80 07",
+		"email":"cfpabeniaziz.mfep@gmail.com",
+		"image":"img/beniaziz.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"سطيف",
+		"denomination":"مركز التكوين المهني والتمهين سطيف إناث",
+		"adresse":"حي المعدومين الخمسة سطيف",
+		"tel":"036 66 28 27",
+		"fax":"036 66 20 99",
+		"email":"Cfpasetiff.mfep@gmail.com",
+		"image":"img/setiff.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"سطيف",
+		"denomination":"مركز التكوين المهني والتمهين عين الطريق",
+		"adresse":"عين الطريق سطيف",
+		"tel":"036 53 09 70",
+		"fax":"036 53 09 71",
+		"email":"cfpaaintrick.mfep@gmail.com",
+		"image":"img/aintrik.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"حمام السخنة",
+		"denomination":"مركز التكوين المهني والتمهين  حمام السخنة",
+		"adresse":"حمام السخنة",
+		"tel":"036 77 84 13",
+		"fax":"036 77 84 13",
+		"email":"Cfpahammamsokhna.mfep@gmail.com",
+		"image":"img/hammamsoukhna.jpg"
+	},
+	{	"type":"CFPA",
+		"commune":"عين لحجر",
+		"denomination":"مركز التكوين المهني والتمهين عين الحجر",
+		"adresse":"عين الحجر",
+		"tel":"036 55 65 38",
+		"fax":"036 55 65 41",
+		"email":"Cfpaainlahdjar.mfep@gmail.com",
+		"image":"img/ainlahjar.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"بئر العرش",
+		"denomination":"مركز التكوين المهني والتمهين بئر العرش",
+		"adresse":"حي 01 نوفمبر 54 بئر العرش",
+		"tel":"036 76 00 43",
+		"fax":"036 76 00 41",
+		"email":"birelarchcfpa@gmail.com",
+		"image":"img/birarch.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"العلمة",
+		"denomination":"مركز التكوين المهني والتمهين العلمة إناث",
+		"adresse":"حي هواري بومدين 19600 العلمة",
+		"tel":"036 76 63 92",
+		"fax":"036 76 63 94",
+		"email":"Cfpaeleulmaf.mfep@gmail.com",
+		"image":"img/eleulmaF.jpg"
+	},
+	{	"type":"CFPA",
+		"commune":"عين آرنات",
+		"denomination":"مركز التكوين المهني والتمهين عين آرنات",
+		"adresse":"عين آرنات",
+		"tel":"036 64 56 03",
+		"fax":"036 64 56 04",
+		"email":"cfpaainarnat.mfep@gmail.com",
+		"image":"img/ainarnat.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"قجال",
+		"denomination":"مركز التكوين المهني والتمهين قجال",
+		"adresse":"رأس الماء قجال",
+		"tel":"036 74 01 05",
+		"fax":"036 74 01 06",
+		"email":"Cfpaguidjel.mfep@gmail.com",
+		"image":"img/guedjel.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"ماوكلان",
+		"denomination":"مركز التكوين المهني والتمهين ماوكلان",
+		"adresse":"ماوكلان",
+		"tel":"036 40 71 24",
+		"fax":"036 40 71 23",
+		"email":"cfpamaoklane660@gmail.com",
+		"image":"img/maoklane.jpg"
+	},
+	{	"type":"CFPA",
+		"commune":"بوعنداس",
+		"denomination":"مركز التكوين المهني والتمهين بوعنداس",
+		"adresse":"بوعنداس",
+		"tel":"036 49 00 35",
+		"fax":"036 49 00 34",
+		"email":"cfpabouandas.mfep@gmail.com",
+		"image":"img/bouandas.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"عين آزال",
+		"denomination":"مركز التكوين المهني والتمهين عين آزال",
+		"adresse":"حي 311 مسكن عين آزال",
+		"tel":"036 68 51 54",
+		"fax":"036 68 51 55",
+		"email":"cfpaainazel.mfep@gmail.com",
+		"image":"img/ainazel.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"صالح باي",
+		"denomination":"مركز التكوين المهني والتمهين صالح باي",
+		"adresse":"صالح باي",
+		"tel":"036 58 61 31",
+		"fax":"036 58 61 38",
+		"email":"cfpasalahbey.mfep@gmail.com",
+		"image":"img/salahbey.jpg"
+	},
+	{	"type":"CFPA",
+		"commune":"بابور",
+		"denomination":"مركز التكوين المهني والتمهين بابور",
+		"adresse":"حي سوق الجمعة بابور",
+		"tel":"036 59 06 49",
+		"fax":"036 59 06 50",
+		"email":"Cfpababor.mfep@gmail.com",
+		"image":"img/babor.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"عين الكبيرة",
+		"denomination":"مركز التكوين المهني والتمهين  عين الكبيرة",
+		"adresse":"طريق شرفة عين الكبيرة",
+		"tel":"036 59 28 23",
+		"fax":"036 59 28 24",
+		"email":"cfpaainelkebira.mfep@gmail.com",
+		"image":"img/ainkebira.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"بني ورثيلان",
+		"denomination":"مركز التكوين المهني والتمهين بني ورثيلان",
+		"adresse":"بني ورثيلان",
+		"tel":"036 49 30 64",
+		"fax":"036 49 31 90",
+		"email":"cfpabeniourtilane.mfep@gmail.com",
+		"image":"img/beniourtilane.jpg"
+	},
+	{	"type":"CFPA",
+		"commune":"قنزات",
+		"denomination":"مركز التكوين المهني والتمهين قنزات ",
+		"adresse":"حي رابية الربيع 50 مسكن قنزات",
+		"tel":"036 40 91 20",
+		"fax":"036 40 91 21",
+		"email":"Cfpaguenzet.mfep@gmail.com",
+		"image":"img/guenzet.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"العلمة",
+		"denomination":"مركز التكوين المهني والتمهين العلمة ذكور",
+		"adresse":"شارع هبيش العلمة",
+		"tel":"036 47 80 99",
+		"fax":"036 47 81 00",
+		"email":"cfpaeleulmag.mfep@gmail.com",
+		"image":"img/eleulmaG.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"العلمة",
+		"denomination":"مركز التكوين المهني والتمهين  العلمة 02",
+		"adresse":"طريق باتنة العلمة 19600 العلمة",
+		"tel":"036 47 97 44",
+		"fax":"036 47 97 43",
+		"email":"cfpaeleulma2.mfep@gmail.com",
+		"image":"img/eulma2.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"بيضاء برج",
+		"denomination":"مركز التكوين المهني والتمهين بيضاء برج",
+		"adresse":"بيضاء برج",
+		"tel":"036 70 97 78",
+		"fax":"036 70 97 77",
+		"email":"cfpabeidabordj.mfep@gmail.com",
+		"image":"img/beidhabordj.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"عين ولمان",
+		"denomination":"مركز التكوين المهني والتمهين عين ولمان ",
+		"adresse":"عين ولمان سطيف",
+		"tel":"036 67 21 64",
+		"fax":"036 67 22 62",
+		"email":"cfpaainoulmene.mfep@gmail.com",
+		"image":"img/ainoulmene.jpg"
+	},
+	{
+		"type":"CFPA",
+		"commune":"عموشة",
+		"denomination":"مركز التكوين المهني والتمهين عموشة",
+		"adresse":"شارع الشهيد مداني عبد الحميد عموشة",
+		"tel":"036 71 67 30",
+		"fax":"036 71 67 31",
+		"email":"cfpaamoucha.mfep@gmail.com",
+		"image":"img/amoucha.jpg"
+	},
+	{
+		"type":"",
+		"commune":"",
+		"denomination":"",
+		"adresse":"",
+		"tel":"",
+		"fax":"",
+		"email":"",
+		"image":""
+	}
+]
+
+
+function showEtabs(commune){
+	console.log(commune);
+	var etabDiv = document.getElementById("setif__info");
+	var isEmpty = etabDiv.innerHTML === "";
+	if (!(isEmpty)){
+		while(etabDiv.firstChild){
+			etabDiv.removeChild(etabDiv.firstChild);
+		}
+	}
+	if (commune == null){
+		commune = "سطيف"
+	}
+	const headerDiv = document.createElement('header');
+			etabDiv.appendChild(headerDiv);
+			headerDiv.classList.add("text-center", "blue-baby", "setif__info--header");
+			headerDiv.innerHTML = '<p> مؤسسات التكوين المهني و التمهين لبلدية '+commune+'</p>';
+	for (var i = 0;i < etabs.length;i++){
+		
+		if (etabs[i].commune == commune){
+			const myDiv = document.createElement('div');
+			myDiv.classList.add("setif__info--etab");
+			etabDiv.appendChild(myDiv);
+			
+			const div1 = document.createElement('div');
+			myDiv.appendChild(div1);
+			div1.classList.add("setif__info--img");
+			const img = document.createElement('img');
+			img.src = etabs[i].image;
+			div1.appendChild(img);
+			
+			const div2= document.createElement('div');
+			myDiv.appendChild(div2);
+			div2.classList.add("setif__info--titles");
+			
+			
+			const par1 = document.createElement("p");
+			//par1.textContent = etabs[i].denomination;
+			par1.innerHTML =  '<span class="fa-stack fa-lg"><i class="fa fa fa-institution fa-stack-1x"></i></span>'+etabs[i].denomination;
+			div2.appendChild(par1);
+			
+			const par2 = document.createElement("p");
+			div2.appendChild(par2);
+			par2.innerHTML =  '<span class="fa-stack fa-lg"><i class="fa fa fa-address-book fa-stack-1x"></i></span>'+etabs[i].adresse;
+			
+			
+			const par3 = document.createElement("p");
+			div2.appendChild(par3);
+			par3.innerHTML = '<span class="fa-stack fa-lg"><i class="fa fa-phone fa-stack-1x"></i></span>'+etabs[i].tel;
+			
+			const par4 = document.createElement("p");
+			div2.appendChild(par4);
+			par4.innerHTML = '<span class="fa-stack fa-lg"><i class="fa fa-fax fa-stack-1x"></i></span>'+etabs[i].fax;
+			
+			const par5 = document.createElement("p");
+			div2.appendChild(par5);
+			par5.innerHTML = '<span class="fa-stack fa-lg"><i class="fa fa-at fa-stack-1x"></i></span>' + etabs[i].email; 
+			
+			
+		}
+	}
+}
+
+
+function addCommLocalStore(param){
+	
+	if (typeof(Storage)!== "undefined") {
+		localStorage.removeItem('lastCom');
+		localStorage.setItem('lastCom',param);
+	}
+
+}
+
+let info =localStorage.getItem('lastCom');
+if (info !== "undefined") {
+	localStorage.removeItem('lastCom');
+	localStorage.setItem('lastCom',"سطيف");
+	showEtabs(info);
+}
+else
+	showEtabs("سطيف");
